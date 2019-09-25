6a6bfb52635ace98d02ced75d81353ca5d8bff38
steve.bachochin <steve.bachochin@truecos.com>
2019-09-25 15:39:37 -0600

    pickers ...

---
+++ Notes/Notes/Models/PickerItems.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes.Models
+{
+    class PickerItems
+    {
+        public int Value { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Notes/Notes/NotesEntryPage.xaml.cs
+++ Notes/Notes/NotesEntryPage.xaml.cs
@@ -7,15 +7,20 @@ namespace Notes
 {
     public partial class NoteEntryPage : ContentPage
     {
+
+        public static string selectedActive = "";
+        public static int selectedItem = 1;
         public NoteEntryPage()
         {
             InitializeComponent();
+            //pickerActive.SelectedItem = //"Capuchin Monkey";
         }
 
         async void OnSaveButtonClicked(object sender, EventArgs e)
         {
             var note = (User_Access)BindingContext;
             //note.Date = DateTime.UtcNow;
+            note.Comments = selectedActive;
             await App.Database.SaveNoteAsync(note);
             await Navigation.PopAsync();
         }
@@ -26,5 +31,24 @@ namespace Notes
             await App.Database.DeleteNoteAsync(note);
             await Navigation.PopAsync();
         }
+
+        void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var oldText = e.OldTextValue;
+            var newText = e.NewTextValue;
+        }
+
+        void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex != -1)
+            {
+                //selectedActive = (string)picker.ItemsSource[selectedIndex];
+                //steveLable.Text = (string)picker.ItemsSource[selectedIndex];
+            }
+        }
+
     }
 }
